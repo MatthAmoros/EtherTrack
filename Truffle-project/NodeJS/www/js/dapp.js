7f477dcd3aac6155676440279eaeb66a5cc52dc7
SunPaz <matth.amoros@gmail.com>
2018-03-29 11:10:30 -0700

    Refactoring dapp.js

---
+++ Truffle-project/NodeJS/www/js/dapp.js
@@ -0,0 +1,62 @@
+var currentAccount;
+function accountUpdate(account)
+{
+	if(currentAccount != account)
+	{
+		currentAccount = account;
+		provider.eth.defaultAccount = account;
+		$("#navbarNavAltMarkup").find("#cntAsAccount").text("User : " + account.substring(0, 10) + "[...]");
+		reloadPreference();
+	}
+}
+
+function startDapp(provider) {
+	if (!provider.isConnected()) {
+		console.log("Not connected")
+		//Metamask needed 
+		$('#main').replaceWith('<div><a href="https://metamask.io/"><img src="./img/metamask-required.png" /></a></div>');
+
+		return;
+	}
+
+	//Account refresh
+	setInterval(() => {
+	web3.eth.getAccounts((err, accounts) => {
+		console.log("Refresh account ...");
+		if (err) return
+		accountUpdate(accounts[0]);
+	})
+	}, 3000);
+	accountUpdate(provider.eth.accounts[0]);
+
+        provider.sendAsync = Web3.providers.HttpProvider.prototype.send;
+	provider.eth.defaultAccount = provider.eth.accounts[0];
+
+    //Get user preferences
+    if (!sessionStorage.accountInformationSent) {
+
+	reloadPreference();
+        sessionStorage.accountInformationSent = 1;
+    }
+}
+
+function reloadPreference()
+{
+	console.log("Reloading preferences...");
+        $.ajax({
+            url: '/accountConnected',
+            type: "POST",
+            data: JSON.stringify({ address: provider.eth.coinbase }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+		bindedContract = [];
+		$('#WHList').empty();
+		$('#NSList').empty();
+
+                console.log("Loading user preferences ...");
+                result.warehouse.forEach(function (wh) { let myWarehouse = new Warehouse(wh.address, null, null, provider); bindedContract.push(myWarehouse);});
+                result.ns.forEach(function (ns) { let myNameService = new NameService(ns.address, provider, null); bindedContract.push(myNameService);});
+            }
+        })
+}
