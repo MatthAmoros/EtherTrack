2b7bdb759228d0ef5ee710fe3459e0518b050d47
SunPaz <matth.amoros@gmail.com>
2018-04-24 04:30:51 -0700

    Updated cloud function to fix connection error

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -12,17 +12,26 @@ admin.initializeApp({
 // Verify token
 // POST to /createUser
 exports.createUser = functions.https.onRequest((req, res) => {	
+	var status;
 	if (req.method === 'POST') {
 		if (req.body.ethAddress !== undefined) {    
 			admin.auth().createCustomToken(req.body.ethAddress, {ethAdd: req.body.ethAddress})
 			.then(function(customToken) {
+				/*
+				  firebase.database().ref('users/' + req.body.ethAddress).set({
+											firstConnection: true
+					});
+				  */
 				// Send token back to client
 				return res.status(200).send({token : customToken, ethAddres: req.body.ethAddress});
 			})
 			.catch(function(error) {
-				return res.status(400).send(ret);
+				return res.status(400).send(error);
 			}); 
 		  }
-			return res.status(400).send(ret);
+		  else
+		  {
+			return res.status(400).send({error : "Undefined ETH Address", success: false});
+		  }
 	}
 });
