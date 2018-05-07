4d0e9f79ebe17b97093f82f43b4bbeab4a2a8ab6
SunPaz <matth.amoros@gmail.com>
2018-05-07 08:59:23 -0700

    Removed _deploy directory (now that we use TravisCI and Firebase hosting...)

--- _deploy/install-node-modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to install NodeJs components
-npm install node
-npm install express
-nmp install express-session
-nmp install express-json
-npm install body-parser
-npm install cookie-parser
-npm install multer
--- _deploy/run-app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to start truffle and node web server
-cd Truffle-project
-truffle compile
-truffle develop | migrate --reset
-cd NodeJS
-node node-server.js
