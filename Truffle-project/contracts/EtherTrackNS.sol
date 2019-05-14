94dfc9d8b13626c59d34e5e656d0ff49783f7740
SunPaz <matth.amoros@gmail.com>
2019-05-14 12:02:37 -0700

    Fixed an issue when registering a name without specifying a node address would fail

--- Truffle-project/contracts/EtherTrackNS.sol
+++ Truffle-project/contracts/EtherTrackNS.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.4.19;
 contract owned
 {
     function owned() public { owner = msg.sender; }
-    function delegateOwnership(address newOwner) public onlyOwner { owner = newOwner; } 
+    function delegateOwnership(address newOwner) public onlyOwner { owner = newOwner; }
     address owner;
-    
+
         modifier onlyOwner {
         require(msg.sender == owner);
         _;
@@ -31,9 +31,9 @@ contract EtherTrackNS is owned {
     }
 
     function createDataStore() public returns (address) {
-		require(_dataStore == address(0));
-        _dataStore = new EtherTrackDataStore();
-		return _dataStore;
+			require(_dataStore == address(0));
+			_dataStore = new EtherTrackDataStore();
+			return _dataStore;
     }
 
     // Datastore
@@ -46,69 +46,67 @@ contract EtherTrackNS is owned {
 
     /// updateRegisters
     /// Upadtes registry with the provided node/name pair and a the secret for futur hashing
-    function updateRegisters(address node, uint64 GS1_GLN) internal
-    {
-		require(_dataStore != address(0));
-        require(!this.exists(node));
-		require((EtherTrackDataStore(_dataStore).getNodebyName(GS1_GLN) == address(0)));
+    function updateRegisters(address node, uint64 GS1_GLN) internal {
+			require(_dataStore != address(0));
+			require(!this.exists(node));
+			require((EtherTrackDataStore(_dataStore).getNodebyName(GS1_GLN) == address(0)));
 
-		//Update data store
-		EtherTrackDataStore(_dataStore).setNamebyNode(node, GS1_GLN);
+			//Update data store
+			EtherTrackDataStore(_dataStore).setNamebyNode(node, GS1_GLN);
 
-		if(_parent != address(0))
-			EtherTrackNS(_parent).registerName(node, GS1_GLN); //Notify parent
+			if(_parent != address(0))
+				EtherTrackNS(_parent).registerName(node, GS1_GLN); //Notify parent
 
-		updateEntries(node, GS1_GLN);        
+			updateEntries(node, GS1_GLN);
     }
 
     /// getNameByNodeAddress
     /// Returns name corresponding to provided node address
-    function getNameByNodeAddress(address node) external view returns(uint64 _name)
-    {
-		require(_dataStore != address(0));
-        _name = EtherTrackDataStore(_dataStore).getNamebyNode(node);
-        
-        if(_parent != address(0) && _name == 0)
-            _name = EtherTrackNS(_parent).getNameByNodeAddress(node);
-        
-        return _name;
+    function getNameByNodeAddress(address node) external view returns(uint64 _name) {
+			require(_dataStore != address(0));
+			_name = EtherTrackDataStore(_dataStore).getNamebyNode(node);
+
+			if(_parent != address(0) && _name == 0)
+				_name = EtherTrackNS(_parent).getNameByNodeAddress(node);
+
+			return _name;
     }
 
     /// getNameByNodeAddress
     /// Returns name corresponding to provided node address
-    function getNodeAddressByName(uint64 GS1_GLN) external view returns(address _node)
-    {
-        _node = EtherTrackDataStore(_dataStore).getNodebyName(GS1_GLN);
-        
-        if(_parent != address(0) && _node == address(0))
-            _node = EtherTrackNS(_parent).getNodeAddressByName(GS1_GLN);
-        
-        return _node;
+    function getNodeAddressByName(uint64 GS1_GLN) external view returns(address _node) {
+			_node = EtherTrackDataStore(_dataStore).getNodebyName(GS1_GLN);
+
+			if(_parent != address(0) && _node == address(0))
+				_node = EtherTrackNS(_parent).getNodeAddressByName(GS1_GLN);
+
+			return _node;
     }
-    
+
     /// getNameByNodeAddress
     /// Returns name corresponding to provided node address
     function exists(address node) external view returns(bool exists)
     {
-		require(_dataStore != address(0));
+			require(_dataStore != address(0));
 
-        bool isRegistered = (EtherTrackDataStore(_dataStore).getNamebyNode(node) != 0);
+			bool isRegistered = (EtherTrackDataStore(_dataStore).getNamebyNode(node) != 0);
 
-        if(!isRegistered && _parent != address(0))
-        {
-            isRegistered = EtherTrackNS(_parent).exists(node);
-        }
-        
-        return isRegistered;
+			if(!isRegistered && _parent != address(0))
+				isRegistered = EtherTrackNS(_parent).exists(node);
+
+
+			return isRegistered;
     }
-    
+
     /// registerName
     /// Registers name and asociate it to caller address
-    function registerName(address node, uint64 GS1_GLN) external payable
-    {
-            updateRegisters(node, GS1_GLN);
+    function registerName(address node, uint64 GS1_GLN) external payable  {
+			if(node == address(0))
+				updateRegisters(owner, GS1_GLN);
+			else
+				updateRegisters(node, GS1_GLN);
     }
-    
+
     function kill() internal onlyOwner {
         //Preserver data
         EtherTrackDataStore(_dataStore).delegateOwnership(owner);
@@ -125,24 +123,24 @@ contract mortal is owned {
 
 /// Storage contract
 contract EtherTrackDataStore is owned, mortal {
-    
+
     /// Hash table that pair address with public name
     mapping(address => uint64) private nameByNode;
     mapping(address => bool) private registeredByNode;
-    mapping(uint64 => address) private nodeByName; 
-    
-    function EtherTrackDataStore() public { 
+    mapping(uint64 => address) private nodeByName;
+
+    function EtherTrackDataStore() public {
 
     }
-    
+
     function getNamebyNode (address node) external view returns (uint64) {
         return nameByNode[node];
     }
-    
+
     function getNodebyName (uint64 name) external view returns (address) {
         return nodeByName[name];
     }
-    
+
     function setNamebyNode (address node, uint64 name) external onlyOwner returns (uint64) {
         require(!registeredByNode[node]);
         nameByNode[node] = name;
